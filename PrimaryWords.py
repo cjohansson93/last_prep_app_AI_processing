40e9d0ceb9e9dfdeabb9e60a878d1514b40ccdd4
cjohansson93 <sir.christianjohansson@gmail.com>
2023-03-05 20:00:17 -0500

    Added comments.

---
+++ PrimaryWords.py
@@ -0,0 +1,21 @@
+"""
+Batool Salloum, Christian Johansson, Inaya Alkhatib, Kevine Shima
+CSI CUE: LSAT Prep App
+2023
+Professor Silveyra
+This file
+"""
+
+import csv
+import json
+
+
+"""
+Main
+"""
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
